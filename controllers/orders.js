7c813eac3598b11727b465544c1c534c251fa44c
AndrewDyomin <dyomin.andrew1@gmail.com>
2024-01-21 18:53:31 +0200

    Update orders.js

--- controllers/orders.js
+++ controllers/orders.js
@@ -36,12 +36,31 @@ async function getHomeIsOrders() {
     return orders;
 };
 
+async function getMilliniOrders() {
+    
+    let orders = [];
+
+    try {
+        await axios.get('https://script.google.com/macros/s/AKfycbw_jmQzhpuKhJojM4smG1_sib9paTgJfiIPEJuDaTzxELnCdQpmVdeafWgXX9L16Ufu/exec')
+        .then((response) => {
+            orders = response.data.orders;
+            return(orders);
+        });
+        
+    } catch(err) {
+        console.log(err)
+    }
+
+    return orders;
+};
+
 async function getAllOrders(req, res, next) {
 
     const mebTownOrders = await getMebTownOrders();
     const homeIsOrders = await getHomeIsOrders();
+    const milliniOrders = await getMilliniOrders();
 
-    const allOrdersArray = mebTownOrders.concat(homeIsOrders);
+    const allOrdersArray = mebTownOrders.concat(homeIsOrders, milliniOrders);
 
     res.status(200).json({ allOrdersArray });
 };
