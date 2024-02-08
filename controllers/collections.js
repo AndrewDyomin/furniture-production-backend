59d0d0880e8bc3667bd684fb7b6fa5096a99f14d
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-02-08 13:10:11 +0200

    Update collections.js

--- controllers/collections.js
+++ controllers/collections.js
@@ -66,7 +66,7 @@ async function remove(req, res, next) {
     const { id } = req.body;
   try {
     await Collection.findByIdAndDelete(id)
-
+    consle.log(id);
     res.status(200).json({ message: "collection was deleted" });
   } catch (error) {
     next(error);
