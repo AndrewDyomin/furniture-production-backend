e8c17f46ca074eafaf3e4b44bc6a33b4eec02688
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-02-23 11:16:04 +0200

    Update orders.js

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -4,10 +4,11 @@ const OrdersController = require("../../controllers/orders");
 const isAuth = require("../../middlewares/isAuth")
 
 const router = express.Router();
+const jsonParser = express.json();
 
 router.get("/all", isAuth, OrdersController.getAllOrders);
-router.post("/add", isAuth, OrdersController.addOrder);
-router.get("/update", isAuth, OrdersController.updateOrder);
+router.post("/add", jsonParser, isAuth, OrdersController.addOrder);
+router.post("/update", jsonParser, isAuth, OrdersController.updateOrder);
 router.delete("/remove", isAuth, OrdersController.deleteOrder);
 
 module.exports = router;
