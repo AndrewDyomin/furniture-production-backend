2e987f24a907e59ea8cbfc288922ce5fd6351cd5
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-08-15 16:30:17 +0300

    Create model.js

---
+++ models/model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const modelSchema = new mongoose.Schema({
+  family: {
+    type: String,
+    required: [true, "Family is required"],
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  size: {
+    type: String,
+    default: "",
+  },
+  sleepingArea: {
+    type: String,
+    default: "",
+  },
+  price: {
+    type: Number,
+    default: 1000,
+  },
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    unique: true,
+  },
+  images: [{
+    type: String,
+  }],
+});
+
+module.exports = mongoose.model("Model", modelSchema);
