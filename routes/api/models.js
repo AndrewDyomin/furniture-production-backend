afd655c0af3ffc7d1e56776e6ac19987a0dc4618
AndrewDyomin <123809505+AndrewDyomin@users.noreply.github.com>
2024-08-22 12:52:55 +0300

    Summary

--- routes/api/models.js
+++ routes/api/models.js
@@ -2,14 +2,15 @@ const express = require("express");
 const { upload } = require("../../middlewares/upload");
 const ModelsController = require("../../controllers/models");
 const isAuth = require("../../middlewares/isAuth");
+const myagkofToDisk = require("../../middlewares/myagkofToDisk");
 
 const router = express.Router();
 const jsonParser = express.json();
 
 router.get("/all", jsonParser, ModelsController.getAll);
 router.post("/get", jsonParser, ModelsController.getOne);
-router.post("/add", jsonParser, isAuth, upload.array("file", 12), ModelsController.add);
-router.post("/update", jsonParser, isAuth, upload.array("file", 12), ModelsController.update);
+router.post("/add", jsonParser, isAuth, upload.array("file", 12), myagkofToDisk, ModelsController.add);
+router.post("/update", jsonParser, isAuth, upload.array("file", 12), myagkofToDisk, ModelsController.update);
 router.delete("/remove", jsonParser, isAuth, ModelsController.remove);
 
 module.exports = router;
